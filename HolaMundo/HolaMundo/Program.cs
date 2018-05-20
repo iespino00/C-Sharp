5c24cca90b8df2f95e02da67a96fef634470e475
iespino <iespino69@gmail.com>
2018-05-20 15:37:07 -0500

    Uploading Hello World

---
+++ HolaMundo/HolaMundo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundo
+{
+    //clase
+    class Program
+    {
+        //Funciones
+        static void Main(string[] args)
+        {
+
+            //Un espacio de memoria que se define para ser utilizado y almacenar datos
+
+
+            //instrucciones
+            Console.Write("Hola Mundo");
+            Console.ReadKey();
+        }
+    }
+}
