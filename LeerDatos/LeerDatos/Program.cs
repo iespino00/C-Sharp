c38c139b683351d87a17ad489272b36deb292b66
iespino <iespino69@gmail.com>
2018-05-20 20:47:18 -0500

    Lectura de datos desde Consola

---
+++ LeerDatos/LeerDatos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeerDatos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pedir datos a un usuario desde consola.
+
+            string entrada = "";
+
+            Console.WriteLine("Escribe tu Nombre: ");
+            entrada = Console.ReadLine();
+
+            Console.WriteLine("Hola: {0} ¿Como te encuentras hoy?" , entrada);
+            Console.ReadKey();
+        }
+    }
+}
