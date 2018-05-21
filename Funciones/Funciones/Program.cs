44f3e4242a1e74934102cdb0b90224783ab144ac
iespino <iespino69@gmail.com>
2018-05-21 11:11:18 -0500

    Funciones con return

---
+++ Funciones/Funciones/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tipos de funciones
+            /*
+                Ejecutan codigo
+                Devuelven valores
+                Reciben valores
+                Reciben valores y devuelven valores 
+             */
+            int n = 5;
+            mostrarMsn();
+            Console.WriteLine(regresaSuma());
+
+            Console.ReadKey();
+        }
+
+        //FUNCION QUE EJECUTA UN CODIGO
+        //Se pone static porque se ejecuta desde el mismo codigo.
+        static void mostrarMsn()
+        {
+            Console.WriteLine("Funcion de ejecutar codigo");
+           
+        }
+
+        //funcion que regresa un valor
+        //tipo de funcion + tipo de valor a regresar + nombre de funcion + parametros dentro de la funcion
+        static int regresaSuma()
+        {
+            int suma = 9 + 9;
+            return suma;
+        }
+
+    }
+}
