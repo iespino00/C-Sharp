4cb1751f5b2b48d4263a27c8c81078e35dfb6cd0
iespino <iespino69@gmail.com>
2018-05-21 13:00:28 -0500

    Stacks o Pilas

---
+++ StackPila/StackPila/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace StackPila
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ArrayList
+            ArrayList carros = new ArrayList();
+            carros.Add("Versa");
+            carros.Add("Pointer");
+            carros.Add("Mustan");
+            carros.Add("Impala");
+
+           for (int i=0; i<carros.Count; i++)
+            {
+                Console.WriteLine(carros[i]);
+            }
+
+            /*
+             * PILA- Es una coleccion de elementos, estructura de tipo LIFO (Last in First Out), el ultimo en entrar es el primero en salir
+            Puede ser dinámica y puede ser de cualquier tipo, se puede restringir poniendo el tipo de dato por ejemplo
+             Stack<String> pilaString = new Stack<String>();
+            Existen dos funciones elementales:
+             push -> insertar elementos dentro de la pila
+             pop  -> devolver el ultimo elemento que se ingreso */
+
+            Stack miPila = new Stack();
+            miPila.Push(8);
+            miPila.Push(10);
+            miPila.Push(20);
+            miPila.Push(30);
+
+            int num = 0;
+
+            /*
+             Como no se definió un tipo a la pila, si no ponemos (int) marcará error porque puede recibir un string , boolean o cualquier tipo
+             entonces con eso nos aseguramos de que será int.
+             */
+            num = (int)miPila.Pop();
+
+            miPila.Clear();
+
+            Console.WriteLine("El Ultimo valor a insertado en la pila es: {0}",num);
+            Console.WriteLine("El número total de elementos de la pila es de: {0}", miPila.Count);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StackPila/StackPila/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("StackPila")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("StackPila")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("db49a375-41f6-41be-8e25-195580aeb046")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
+// mediante el carácter '*', como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
