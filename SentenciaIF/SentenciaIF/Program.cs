8eb81b5bc633ccda239feb580d9025c00b1322d1
iespino <iespino69@gmail.com>
2018-05-21 09:10:04 -0500

    Sentencia IF

---
+++ SentenciaIF/SentenciaIF/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentenciaIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IF
+
+            if(10 > 5)
+            {
+                //Instrucciones
+                Console.Write("10 si es mayor a 5");
+            }
+            Console.ReadKey();
+        }
+    }
+}
