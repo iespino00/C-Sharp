6d81fc8f1de07b39cd54c2ff54447bf1aca64ffb
iespino <iespino69@gmail.com>
2018-05-21 09:32:02 -0500

    Arrays

---
+++ Arreglos/Arreglos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //   definicion de un arreglo
+            //  tipo[] nombre = new tipo[tamaño];
+
+            int[] arreglo = new int[4];
+
+            arreglo[0] = 10;
+            arreglo[1] = 20;
+            arreglo[2] = 30;
+            arreglo[3] = 40;
+
+            Console.WriteLine("El valor de la posición 0 es: {0}", arreglo[0]);
+            Console.ReadKey();
+
+        }
+    }
+}
