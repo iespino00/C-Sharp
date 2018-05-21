89a82a59047a841247df8e3c67f6fc614509d215
iespino <iespino69@gmail.com>
2018-05-21 10:50:46 -0500

    Ciclo While

---
+++ Ciclos/Ciclos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciclos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ciclos son bloques de codigo que se repiten un determinado numero
+
+            //While -> Mientras
+            int x = 0;
+            while(x<=10)
+            { 
+            Console.WriteLine("Hello World {0}",x);
+                x++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
