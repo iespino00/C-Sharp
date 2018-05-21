4ca88c3d98ce5eba35b1ae7fd70e2791aba2a3ab
iespino <iespino69@gmail.com>
2018-05-21 16:03:11 -0500

    Creando Objetos de Clase

---
+++ POO/POO/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    class Principal
+    {
+        static void Main(String[] args)
+        {
+            Automovil auto = new Automovil();
+            auto.precio = 5000;
+            Console.WriteLine("Precio del Auto: {0}", auto.precio);
+            auto.arrancar();
+            Console.ReadKey();
+        }
+
+    }
+
+    class Automovil
+    {
+        //Atributos
+        //Modificador de acceso, tipo y nombre
+        public int precio;
+        public int velocidad;
+
+        //metodos o funciones
+        public void arrancar()
+        {
+            Console.WriteLine("El Automovil está arrancando...");
+        }
+
+    }
+
+
+}
