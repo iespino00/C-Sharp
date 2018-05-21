ef15b1da6d43da8241ae6079f285e8c045ea95b1
iespino <iespino69@gmail.com>
2018-05-20 20:21:12 -0500

    Upload: Impresion de variables en Consola

--- Variables/HolaMundo/Program.cs
+++ Variables/HolaMundo/Program.cs
@@ -14,10 +14,15 @@ namespace HolaMundo
         {
 
             //Un espacio de memoria que se define para ser utilizado y almacenar datos
-
+            int numero = 5;
+            string texto = "mi cadena";
+            bool boleana = false;
 
             //instrucciones
-            Console.Write("Hola Mundo");
+            //  Console.Write("La variable Entera tiene: {0} y la variable cadena contiene: {1} y la variable booleana contiene: {2}", numero,texto,boleana);
+            Console.Write("La variable Entera tiene: {0}",numero);
+            Console.Write("La variable string contiene: {0}", texto);
+            Console.Write("La variable boleana contiene: {0}", boleana);
             Console.ReadKey();
         }
     }
