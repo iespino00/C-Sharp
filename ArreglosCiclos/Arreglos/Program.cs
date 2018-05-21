f83fb2f172cf95456fe1edc584fd6ddc84fca8af
iespino <iespino69@gmail.com>
2018-05-21 09:41:14 -0500

    Llenado de Arreglos con ciclo FOR

---
+++ ArreglosCiclos/Arreglos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //   definicion de un arreglo
+            //  tipo[] nombre = new tipo[tamaño];
+
+            int[] arreglo = new int[10];
+
+
+
+
+            for(int i=0; i<10; i++)
+            {
+                arreglo[i] = i+1;
+                Console.WriteLine("El valor de la posición {0} es: {1}",i, arreglo[i]);
+            }
+
+           
+            Console.ReadKey();
+
+        }
+    }
+}
